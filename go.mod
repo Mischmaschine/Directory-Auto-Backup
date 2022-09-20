919b5c61f9d88447ca365c19a2a9750520f9b95d
Mischmaschine <fixxertrixxer@gmail.com>
2022-09-20 21:45:36 +0200

    feat: add SFTPConnectionManager.go

--- go.mod
+++ go.mod
@@ -3,3 +3,11 @@ module Directory-Auto-Backup
 go 1.19
 
 require gopkg.in/yaml.v2 v2.2.8
+
+require (
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/pkg/sftp v1.13.5 // indirect
+	github.com/procyon-projects/chrono v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20220919173607-35f4265a4bc0 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"bufio"
 	_ "context"
 	"encoding/json"
 	"fmt"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
 	"io/ioutil"
 	_ "log"
+	"net"
+	"net/url"
 	"os"
 	_ "time"
 )
@@ -14,6 +20,71 @@ func main() {
 
 	fmt.Println("Thanks for using the Auto-Backup tool! This tool will automatically backup your files to a remote server.")
 	fmt.Println("Trying to read config file [config.json]...")
+
+	// Get SFTP To Go URL from environment
+	rawurl := os.Getenv("SFTPTOGO_URL")
+
+	parsedUrl, err := url.Parse(rawurl)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse SFTP To Go URL: %s\n", err)
+		os.Exit(1)
+	}
+
+	// Get user name and pass
+	user := parsedUrl.User.Username()
+	pass, _ := parsedUrl.User.Password()
+
+	// Parse Host and Port
+	host := parsedUrl.Host
+	// Default SFTP port
+	port := 22
+
+	hostKey := getHostKey(host)
+
+	fmt.Fprintf(os.Stdout, "Connecting to %s ...\n", host)
+
+	var auths []ssh.AuthMethod
+
+	// Try to use $SSH_AUTH_SOCK which contains the path of the unix file socket that the sshd agent uses
+	// for communication with other processes.
+	if aconn, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
+		auths = append(auths, ssh.PublicKeysCallback(agent.NewClient(aconn).Signers))
+	}
+
+	// Use password authentication if provided
+	if pass != "" {
+		auths = append(auths, ssh.Password(pass))
+	}
+
+	// Initialize client configuration
+	remoteConfig := ssh.ClientConfig{
+		User: user,
+		Auth: auths,
+		// Uncomment to ignore host key check
+		//HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		HostKeyCallback: ssh.FixedHostKey(hostKey),
+	}
+
+	addr := fmt.Sprintf("%s:%d", host, port)
+
+	// Connect to server
+	conn, err := ssh.Dial("tcp", addr, &remoteConfig)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to connecto to [%s]: %v\n", addr, err)
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
+	// Create new SFTP client
+	sc, err := sftp.NewClient(conn)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to start SFTP subsystem: %v\n", err)
+		os.Exit(1)
+	}
+	defer sc.Close()
+
+	test()
 
 	jsonFile, err := os.Open("users.json")
 	if err != nil {
@@ -30,6 +101,7 @@ func main() {
 		prettyJSON, err := json.MarshalIndent(Config{
 			RemotePath: "",
 			LocalPaths: []string{"/Users/max/Downloads", "/Users/max/Downloads"},
+			Delay:      4,
 		}, "", "    ")
 		if err != nil {
 			fmt.Println(err)
@@ -60,9 +132,18 @@ func main() {
 		fmt.Println(config.LocalPaths[i])
 	}
 
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		fmt.Println("> Echo:", input.Text())
+	}
+
 }
 
 type Config struct {
-	RemotePath string   `json:"remote_path"`
+	IP         string   `json:"remote_ip"`
+	Password   string   `json:"remote_password"`
+	Port       int      `json:"remote_port"`
+	RemotePath string   `json:"remote_save_path"`
 	LocalPaths []string `json:"local_path"`
+	Delay      int      `json:"delay"`
 }
